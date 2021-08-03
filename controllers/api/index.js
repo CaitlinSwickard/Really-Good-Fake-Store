c95134ca154df627c5a1c5dbea79b54409ed780a
Ana Ea <aabad0464@gmail.com>
2021-08-02 21:49:33 -0600

    get api route

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const customerRoutes = require('./customerRoutes');
+
+
+router.use('/customer', customerRoutes);
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 // dependencies
 const express = require('express');
-// const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 // express app
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
-// app.use(routes);
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({force:false}).then(()=>{
