5a950649ec55c17d178eeab92423901ec6a62d8b
COcoder555 <cocoder555@gmail.com>
2021-08-02 19:22:43 -0600

    Added models

---
+++ models/Cart.js
@@ -0,0 +1,15 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+class Cart extends Model {}
+Cart.init(
+  {
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'cart',
+  }
+);
+module.exports = Cart
