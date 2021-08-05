92df2880a6a7cc1175f96caa084bf76f47ef817e
Caitlin Swickard <caitlin6029@gmail.com>
2021-08-04 19:27:17 -0600

    filled in models

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -1,15 +1,51 @@
-// const { Model, DataTypes } = require('sequelize');
-// const sequelize = require('../config/connection');
-// class Wishlist extends Model {}
-// Wishlist.init(
-//   {
-//   },
-//   {
-//     sequelize,
-//     timestamps: false,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'wishlist',
-//   }
-// );
-// module.exports = Wishlist
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Wishlist extends Model {}
+
+Wishlist.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    customer_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model:'customer',
+        key:'id',
+      }
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'wishlist',
+  }
+);
+
+module.exports = Wishlist
--- models/index.js
+++ models/index.js
@@ -2,4 +2,27 @@ const Cart = require('./Cart');
 const Customer = require('./Customer');
 const Wishlist = require('./Wishlist');
 
+
+Customer.hasMany(Cart, {
+  foreignKey: 'customer_id',
+  onDelete: 'CASCADE'
+});
+
+Customer.hasMany(Wishlist, {
+  foreignKey: 'customer_id',
+  onDelete: 'CASCADE'
+})
+
+Cart.belongsTo(Customer, {
+  foreignKey: 'customer_id'
+});
+
+Wishlist.belongsTo(Customer, {
+  foreignKey: 'customer_id'
+});
+
+
+
 module.exports = { Cart, Customer, Wishlist};
+
+
