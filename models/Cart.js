2784dddf63ed9322502bbd87fb8595963619b6f5
Maximo Sandoval <80641742+maximosandoval@users.noreply.github.com>
2021-08-09 14:58:29 -0600

    Created shop and cart pages

--- models/Cart.js
+++ models/Cart.js
@@ -13,3 +13,16 @@
 //   }
 // );
 // module.exports = Cart
+
+//Add to cart router.get('/cart/:id', function(req,res)){
+// var productId = req.productId.productId;
+// });
+
+module.exports = function Cart(initItems) {
+  this.items = initItems; //items in the cart
+  this.totalQty = 0; //total item qty in cart
+  this.totalPrice = 0; //total price in cart
+  this.add = function (item, id) {
+    var storedItem = this.items(id);
+  };
+};
